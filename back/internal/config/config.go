48d742e363e799c9092c0cb7c5bc49ee56412bc1
MohammadBnei <mohammadamine.banaei@pm.me>
2025-03-08 14:14:38 +0100

    lil updates

--- back/internal/config/config.go
+++ back/internal/config/config.go
@@ -7,7 +7,7 @@ import (
 type Config struct {
 	AppName     string `env:"APP_NAME" envDefault:"Voc on Steroid"`
 	HTTPPort    string `env:"HTTP_PORT" envDefault:"8080"`
-	LogLevel    string `env:"LOG_LEVEL" envDefault:"debug"`
+	LogLevel    string `env:"LOG_LEVEL" envDefault:"info"`
 	DatabaseURL string `env:"DATABASE_URL" envDefault:"postgres://user:password@localhost:5432/voconsteroid"`
 	RedisURL    string `env:"REDIS_URL" envDefault:"redis://localhost:6379"`
 	JWTSecret   string `env:"JWT_SECRET" envDefault:"secret"`
