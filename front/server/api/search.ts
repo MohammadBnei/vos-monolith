1f513c5e4404549941f3cedf57da9386ebc4a8fb
Leïla Lharch <llharch@groupeastek.fr>
2025-03-16 13:47:57 +0100

    feat : handle error 404 from api

--- front/server/api/search.ts
+++ front/server/api/search.ts
@@ -2,14 +2,25 @@ export default defineEventHandler(async (event: Event) => {
   const config = useRuntimeConfig(event);
   const body = await readBody(event);
 
-  const result = await $fetch(`/v1/words/search`, {
-    method: "POST",
-    baseURL: config.public.apiBase,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
+  try {
+    const result = await $fetch(`/v1/words/search`, {
+      method: "POST",
+      baseURL: config.public.apiBase,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-  return result;
+    return result;
+  } catch (err) {
+    if (err.response && err.response.status === 404) {
+      return {
+        error: "Word not found",
+      };
+    }
+    return {
+      error: "An unexpected error occurred",
+    };
+  }
 });
