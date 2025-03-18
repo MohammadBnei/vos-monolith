d5a82e01cfc28621465f9e853a51558bb0cdaf70
MohammadBnei <mohammadamine.banaei@gmail.com>
2025-03-18 23:32:02 +0100

    refactor: Rename word import to wordDomain in wiktionary_test.go

--- back/internal/infrastructure/dictionary/wiktionary_test.go
+++ back/internal/infrastructure/dictionary/wiktionary_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
-	"voconsteroid/internal/domain/word"
+	wordDomain "voconsteroid/internal/domain/word"
 	"voconsteroid/internal/infrastructure/dictionary/acl"
 )
 
